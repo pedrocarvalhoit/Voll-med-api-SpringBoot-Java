f0da836ea0be508ab76649cc9f97cb3fee979a26
Pedro Duarte <pedrohdc@yahoo.com.br>
2023-02-02 18:01:03 +0000

    Dados Detalhamento Medico

--- src/main/java/med/voll/api/domain/medico/DadosDetalhamentoMedico.java
+++ src/main/java/med/voll/api/domain/medico/DadosDetalhamentoMedico.java
@@ -1,8 +1,6 @@
-package med.voll.api.dto;
+package med.voll.api.domain.medico;
 
-import med.voll.api.model.Endereco;
-import med.voll.api.model.Especialidade;
-import med.voll.api.model.Medico;
+import med.voll.api.domain.endereco.Endereco;
 
 public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm, String telefone,
                                       Especialidade especialidade, Endereco endereco) {
